02ec71371d6f1129ab11e03479086e85530ab0f2
the-sahil-gupta <sahilgtech95@gmail.com>
2025-01-06 05:00:02 +0530

    all pages completed

---
+++ script-single-project.js
@@ -0,0 +1,205 @@
+//  ==================================== LOADER OF OTHER PAGES ====================================
+function loadingAndHeadingAnimation() {
+	gsap.registerPlugin(ScrollTrigger);
+
+	const loader = document.querySelector('#loader');
+	const timer = document.querySelector('#timer');
+	const headingTitle = document.querySelector('.heading-title');
+	gsap
+		.timeline()
+		.fromTo(
+			loader,
+			{
+				y: '90%',
+			},
+			{
+				delay: -0.2,
+				ease: 'power2.inOut',
+				y: '-110%',
+				duration: 2,
+			}
+		)
+		.fromTo(
+			headingTitle,
+			{
+				clipPath: 'inset(00% 0% 100% 0%)',
+				y: '100%',
+				ease: 'power4.inOut',
+				duration: 1,
+			},
+			{
+				clipPath: 'inset(0% 0% 0% 0%)',
+				y: 0,
+				ease: 'power4.inOut',
+				duration: 1,
+			}
+		);
+}
+loadingAndHeadingAnimation();
+
+//  ==================================== MENU BAR ====================================
+function menuBar() {
+	const togglebtn = document.querySelector('#menu-toggle');
+	const menu = document.querySelector('#menu');
+	const menuContainer = menu.querySelector('.menu-container');
+
+	const links = menuContainer.querySelectorAll(
+		'.menu-links-wrapper .menu-links'
+	);
+
+	togglebtn.addEventListener('click', () => {
+		const label = togglebtn.querySelector('.para-12');
+		if (label.textContent === 'menu') {
+			label.textContent = 'close';
+			menu.style.display = 'block';
+
+			gsap
+				.timeline()
+				.to(menuContainer, {
+					y: 0,
+					duration: 0.8,
+					ease: 'power4.inOut',
+				})
+				.fromTo(
+					links,
+					{
+						clipPath: 'inset(100% 0% 0% 0%)',
+						y: '-100%',
+						stagger: 0.05,
+						duration: 0.2,
+						ease: 'power4.inOut',
+					},
+					{
+						clipPath: 'inset(0% 0% 0% 0%)',
+						y: 0,
+						stagger: 0.05,
+						duration: 0.2,
+						ease: 'power4.inOut',
+					}
+				);
+		} else {
+			label.textContent = 'menu';
+			gsap
+				.timeline()
+				.fromTo(
+					links,
+					{
+						clipPath: 'inset(0% 0% 0% 0%)',
+						y: 0,
+						stagger: 0.05,
+						duration: 0.2,
+						ease: 'power4.inOut',
+					},
+					{
+						clipPath: 'inset(100% 0% 0% 0%)',
+						y: '-100%',
+						stagger: 0.05,
+						duration: 0.2,
+						ease: 'power4.inOut',
+					}
+				)
+				.to(menuContainer, {
+					y: '-100%',
+					duration: 0.8,
+					ease: 'power4.inOut',
+					// clipPath: 'inset(0% 0% 100% 0%)',
+
+					onComplete: () => {
+						menu.style.display = 'none';
+					},
+				});
+		}
+	});
+}
+menuBar();
+//  ==================================== SPLIT CLIPPATH ANIME ====================================
+function textClipPathAnime() {
+	const fullText = document.querySelectorAll('.split-text-clip');
+	fullText.forEach((text) => {
+		text.innerHTML = text.textContent
+			.split('')
+			.map((letter) => `<span>${letter}</span>`)
+			.join('');
+		const spans = text.querySelectorAll('span');
+
+		gsap
+			.timeline({
+				scrollTrigger: {
+					trigger: text,
+					start: 'top bottom',
+					end: 'bottom top',
+					// // markers: true,
+				},
+			})
+
+			.fromTo(
+				spans,
+				{
+					clipPath: 'inset(0% 0% 100% 0%)',
+					// y: '100%',
+					stagger: 0.01,
+					duration: 1.2,
+					ease: 'power4.inOut',
+				},
+				{
+					clipPath: 'inset(0% 0% 0% 0%)',
+					// y: '00%',
+					stagger: 0.01,
+					duration: 1.2,
+					ease: 'power4.inOut',
+				}
+			);
+	});
+}
+textClipPathAnime();
+function elemClipPathAnime() {
+	const elements = document.querySelectorAll('.split-elem-clip');
+	elements.forEach((elem) => {
+		gsap
+			.timeline({
+				scrollTrigger: {
+					trigger: elem,
+					start: 'top bottom',
+					end: 'bottom top',
+					// // markers: true,
+				},
+			})
+
+			.fromTo(
+				elem,
+				{
+					clipPath: 'inset(-30% -30% 130% -30%)',
+					y: '100%',
+					duration: 1.5,
+					ease: 'power4.inOut',
+				},
+				{
+					clipPath: 'inset(-30% -30% -30% -30%)',
+					y: '00%',
+					duration: 1.5,
+					ease: 'power4.inOut',
+				}
+			);
+	});
+}
+elemClipPathAnime();
+
+//  ==================================== PROJECT DETAILS PIN ANIME ====================================
+function pinDetailsAnime() {
+	const detailsContainer = document.querySelector(
+		'.single-project-details-container'
+	);
+	const detailsCard = detailsContainer.querySelector('.s-project-details');
+
+	gsap.to(detailsCard, {
+		scrollTrigger: {
+			trigger: detailsCard,
+			pin: true,
+			scrub: 1,
+			start: 'top-=65 top',
+			end: 'bottom+=200 top',
+			// markers: true,
+		},
+	});
+}
+pinDetailsAnime();
